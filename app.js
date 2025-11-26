6319c009ecfbbee7153c7a502f57f566e9d6cd55
Karlaleticiaavila <Kbliaass@gmail.com>
2025-11-25 17:38:38 -0800

    exercises jquery

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 $(function() {
-  // YOUR CODE HERE
-})
+  $('#toggleButton').on('click', function() {
+    $('#myParagraph').toggle();
+  })
+  })
+
+
+$(function() {
+  $('#colorButton').on('click', function() {
+    $('#colorDiv').css('background-color', 'pink');
+  })
+  })
+$(function() {
+  $('#addClassButton').on('click', function() {
+    $('#classDiv').addClass('newClass');
+  });
+
+  $('#removeClassButton').on('click', function() {
+    $('#classDiv').removeClass('newClass');
+  });
+});
+
+
+$(function() {
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn();
+  });
+
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut();
+  })});
+
+
+  $(function() {
+    $('#slideDownButton').on('click', function() {
+      $('#slideDiv').slideDown();
+    });
+
+    $('#slideUpButton').on('click', function() {
+      $('#slideDiv').slideUp();
+    })});
